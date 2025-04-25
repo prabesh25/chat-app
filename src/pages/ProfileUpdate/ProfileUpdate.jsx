1f944893b207c582cd1c4ababbf5116572e891be
Prabesh <prabesh5511@gmail.com>
2025-04-26 04:20:44 +0545

    add new feature

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
   return (
-    <div>we are on profile page</div>
+    <div className='profile'>
+        <div className="profile-container">
+            <form>
+                <h3>Profile Details</h3>
+                <label htmlFUpload profile imageor="avatar">
+                    <input type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+                    <img src={assets.avatar_icon} alt="" />
+
+                </label>
+                <input type="text" placeholder='Your name' required />
+                <textarea placeholder='write profile bio' required></textarea>
+                <button type='submit'>save</button>
+            </form>
+            <img src={assets.logo_icon} alt="" />
+        </div>
+    </div>
   )
 }
 
