6b1765ca3736ff5fe09976efb19eb03e26daf0d8
Prabesh <prabesh5511@gmail.com>
2025-04-26 06:12:32 +0545

    add all features completed

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProfileUpdate.css'
 import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
+    const [image, setImage] = useState(false)
   return (
     <div className='profile'>
         <div className="profile-container">
             <form>
                 <h3>Profile Details</h3>
                 <label htmlFUpload profile imageor="avatar">
-                    <input type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
-                    <img src={assets.avatar_icon} alt="" />
+                    <input onChange={(e) => setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+                    <img src={image? URL.createObjectURL(image) :  assets.avatar_icon} alt="" />
 
                 </label>
                 <input type="text" placeholder='Your name' required />
                 <textarea placeholder='write profile bio' required></textarea>
                 <button type='submit'>save</button>
             </form>
-            <img src={assets.logo_icon} alt="" />
+            <img className='profile-pic' src={image? URL.createObjectURL(image) : assets.logo_icon} alt="" />
         </div>
     </div>
   )
